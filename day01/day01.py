070685f3ae594dae05d07adf2e55033329e20af6
Michael Hladun <mp.hladun@gmail.com>
2020-12-01 17:48:19 -0500

    fix: day01 improvement

--- day01/day01.py
+++ day01/day01.py
@@ -1,11 +1,11 @@
 
-def func(l):
-  for i, x in enumerate(l):
-    for j, y in enumerate(l[i+1:]):
-      if x + y == 2020:
-        print("x    y    x*y")
-        print(x, y, x*y)
-        return x*y
+SUM = 2020
+
+def part1(l):
+  diffs = {SUM - i for i in l}
+  for x in diffs:
+    if x in l:
+      return (SUM - x) * x
 
 l = [
   1721,
@@ -15,19 +15,24 @@ l = [
   675,
   1456
 ]
-assert func(l) == 514579
 
-def func2(l):
-  for i, x in enumerate(l):
-    for j, y in enumerate(l[i+1:]):
-      for k, z in enumerate(l[j:]):
-        if x + y + z == 2020:
-          print(x, y, z, x * y *z)
-          return
+assert part1(l) == 514579
+
+
+def part2(l):
+  diffs = { 
+            SUM - i - j: (i, j)
+            for i in l
+            for j in l
+          }
+  for x in l:
+    if x in diffs:
+      i, j = diffs[x]
+      return i * j * x
 
 
 with open('day01.txt') as f:
   lines = [int(line.strip()) for line in f.readlines()]
-  func(lines)
-  func2(lines)
+  assert part1(lines) == 1006875
+  assert part2(lines) == 165026160
 
